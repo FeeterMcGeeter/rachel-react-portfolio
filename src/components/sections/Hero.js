9dece5646bf3230e2d4c1235ed8f20e75bfea3eb
Jeffrey Davis <davisj0214@yahoo.com>
2020-06-05 14:37:48 -0500

    hero section markup added

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className='hero'>
-            Hero
+        <div className='hero mb-lg'>
+            <div className='hero__content'>
+                <h1 className='hero__title'>eCommerce Title Stuff Here</h1>
+                <p className='hero__subtitle mb-sm'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus laboriosam voluptatum.
+                </p>
+                <button className='hero__button'>Call to Action</button>
+            </div>
         </div>
     );
 };
