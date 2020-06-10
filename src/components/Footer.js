a7a785a1b7ee14a5d8fff6723b0705d76d38cb1b
Jeffrey Davis <davisj0214@yahoo.com>
2020-06-10 07:41:31 -0500

    added footer with styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Link } from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll';
 
 const Footer = () => {
+
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+
     return (
         <div className='footer'>
-            <Link
-                to='hero'
-                spy={true}
-                smooth={true}
-                duration={500}
+            <div
                 className='footer__btn-toTop'
+                onClick={scrollToTop}
             >
                 <i className="fas fa-chevron-up" />
-            </Link>
+            </div>
+
+            <div className="footer__content">
+                <p className="footer__logo">
+                    rachel<span>DAVIS</span>
+                </p>
+
+                <p className="footer__powered-by">
+                    Powered by
+                    <span>
+                        <i className='fab fa-react' />
+                    </span>
+                    React
+                </p>
+
+                <p className="footer__copyright">
+                    created by
+                    <span>Jeffrey Davis</span>
+                    &copy; 2020
+                </p>
+            </div>
         </div>
     );
 };
